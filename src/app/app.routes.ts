5a45931c1f333fa250743dc3eaf300c253e3ec9e
AxelBlois <axel.blois@edu.esiee.fr>
2025-11-10 11:55:48 +0100

    UserProfile inter, manque finalisation sidebar & header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,32 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { authGuard } from './core/auth/auth-guard';
-import { Component } from '@angular/core';
-import { HomeComponent } from './features/home/home.component';
-import { ProposalsComponent } from './features/proposals/proposals.component';
-import { UserProfileComponent } from './features/user-profile/user-profile.component';
-import { DashboardComponent } from './features/dashboard/dashboard.component';
-
+import {Routes} from '@angular/router';
+import {LoginComponent} from './features/auth/login/login.component';
+import {RegisterComponent} from './features/auth/register/register.component';
+import {authGuard} from './core/auth/auth-guard';
+import {Component} from '@angular/core';
+import {HomeComponent} from './features/home/home.component';
+import {ProposalsComponent} from './features/proposals/proposals.component';
+import {UserProfileComponent} from './features/user-profile/user-profile.component';
+import {DashboardComponent} from './features/dashboard/dashboard.component';
+import {HeaderComponent} from './core/layout/header/header.component';
+import {MainLayoutComponent} from './core/layout/main-layout/main-layout.component';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'auth/login' },
-  { path: 'auth/login', component: LoginComponent },
-  { path: 'auth/register', component: RegisterComponent },
-  { path: 'home',component:HomeComponent},
-  {path:'proposals',component:ProposalsComponent},
-  { path: 'userProfile', component: UserProfileComponent,},
-  {path:'dashboard',component:DashboardComponent},
-  { path: '**', redirectTo: 'auth/login' }
+  { path: '', pathMatch: 'full', redirectTo: 'auth/login'},
+  { path: 'auth/login', component: LoginComponent},
+  { path: 'auth/register', component: RegisterComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'proposals', component: ProposalsComponent},
+  { path: 'dashboard', component: DashboardComponent},
+
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'userProfile', component: UserProfileComponent },
+    ]
+  },
+
+  { path: '**', redirectTo: 'auth/login'}
 ];
+
+
