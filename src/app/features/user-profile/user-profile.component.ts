f7e5c6c6f4293375438caccd8f30e2b4ac7bf4e6
AxelBlois <axel.blois@edu.esiee.fr>
2025-11-10 11:26:55 +0100

    UserProfile composant principal

--- src/app/features/user-profile/user-profile.component.ts
+++ src/app/features/user-profile/user-profile.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Router, RouterLink} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+
+interface User {
+  lastName: string;
+  firstName: string;
+  username: string;
+  mail: string;
+  phoneNumber: string;
+  birthday: string;
+  location: Location;
+}
+
+interface Location {
+  city: string;
+  country: string;
+}
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
+  user: User | null = null;
 
+  constructor() {}
+
+  ngOnInit() {
+    this.user = {
+      lastName: 'François',
+      firstName: 'Hollande',
+      username: 'Flamby',
+      mail: 'flamby@gmail.com',
+      phoneNumber: '0123456789',
+      birthday: '02/22/2002',
+      location: { country: 'France', city: 'Roubaix'}
+    };
+  }
+
+  onSubmit() {
+    if(this.user) console.log(this.user);
+  }
 }
